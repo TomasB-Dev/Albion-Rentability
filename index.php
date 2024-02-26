80e7fb68b44ea4709c249ebd1fb680a2744deb5d
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2024-02-26 20:07:40 -0300

    Add comments in English

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="styles.css" rel="stylesheet">
-    <title>Calculadora de Elaboración en Albion</title>
+    <title>Calculadora de Precios en Albion</title>
 </head>
 <body>
-    <h1>Calculadora de Elaboración en Albion</h1>
+    <h1>Calculadora de Precios en Albion</h1>
 
     <form id="albionForm">
         <label for="itemSelect">Selecciona un ítem:</label>
@@ -49,6 +49,10 @@
     <div id="resultados" class="resultados"></div>
 
     <script>
+        /**
+        * The function `realizarSolicitudAPI` fetches data from an API based on selected item, quality,
+        * enchantment, and tier, and then displays the results.
+        */
         function realizarSolicitudAPI() {
             var itemSeleccionado = document.getElementById('itemSelect').value;
             var calidadSeleccionada = document.getElementById('calidadSelect').value;
@@ -59,12 +63,23 @@
             var api_url = 'https://west.albion-online-data.com/api/v2/stats/prices/';
             var url_solicitud = `${api_url}${tier}${itemSeleccionado}@${encantamientoSeleccionado}.json?qualities=${calidadSeleccionada}`;
 
+            /* This code snippet is making an asynchronous HTTP request using the `fetch` API to the
+            URL specified in `url_solicitud`. Here's a breakdown of what each part does: */
             fetch(url_solicitud)
                 .then(response => response.json())
                 .then(data => mostrarResultados(data, calidadSeleccionada))
                 .catch(error => console.error('Error al realizar la solicitud API:', error));
         }
 
+        /**
+         * The function "mostrarResultados" creates HTML elements to display market data for cities,
+         * including prices for buying and selling items in a game.
+         * 
+         * @return The `mostrarResultados` function is creating HTML elements to display market data
+         * for each city in the provided `data` array. The function formats the prices using the
+         * `formatMoney` function before displaying them. The `formatMoney` function formats the amount
+         * as currency in USD without decimal fractions.
+         */
         function mostrarResultados(data, calidad) {
             var resultadosDiv = document.getElementById('resultados');
             resultadosDiv.innerHTML = '';
@@ -93,6 +108,7 @@
                 function formatMoney(amount) {
                     return amount.toLocaleString(undefined, { style: 'currency', currency: 'USD', minimumFractionDigits: 0, maximumFractionDigits: 0 });
                 }
+                
                 mercadoDiv.appendChild(nombreMercadoP);
                 mercadoDiv.appendChild(precioMinVentaP);
                 mercadoDiv.appendChild(precioMaxVentaP);
